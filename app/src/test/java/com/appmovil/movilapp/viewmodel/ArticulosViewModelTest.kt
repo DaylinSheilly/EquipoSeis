aa03c2251b69df01548b73e3235cebb3789b3dbd
Sheilly Ortega 2040051-3743 <81594679+DaylinSheilly@users.noreply.github.com>
2023-12-15 15:20:38 -0500

    Test guardarProducto

---
+++ app/src/test/java/com/appmovil/movilapp/viewmodel/ArticulosViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.appmovil.movilapp.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.appmovil.movilapp.model.Articulo
+import com.appmovil.movilapp.repository.ArticuloRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+class ArticulosViewModelTest {
+    @get:Rule
+
+    val rule = InstantTaskExecutorRule()
+    private lateinit var articulosViewModel: ArticulosViewModel
+    private lateinit var articuloRepository: ArticuloRepository
+
+    @Before
+
+    fun setUp(){
+        articuloRepository = mock(ArticuloRepository::class.java)
+        articulosViewModel = ArticulosViewModel(articuloRepository)
+    }
+
+    @Test
+    fun`test metodo guardarProducto`() = runBlocking{
+
+        //given
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        val articulo = Articulo(1, "peine", 7000, 3)
+
+        `when`(articuloRepository.guardarProducto(articulo))
+            .thenAnswer{ invocation ->
+                val articuloArgument = invocation.getArgument<Articulo>(0)
+                articuloArgument
+            }
+        //when
+        articulosViewModel.guardarProducto(1.toString(),"peine", 7000.toString(), 3.toString())
+        //then
+        Mockito.verify(articuloRepository).guardarProducto(articulo)
+        Dispatchers.resetMain()
+
+    }
+}
